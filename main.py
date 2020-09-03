1b9bb95ccb7cd2cec0f32d8f2cb830c5c7ba9620
Mateusz <234087@edu.p.lodz.pl>
2020-09-03 17:42:02 +0200

    add exercise 3 except evaluation function

--- main.py
+++ main.py
@@ -8,46 +8,102 @@ from custom_evolutionary_algorithms import main
 from functions import *
 import matplotlib.pyplot as plt
 from xmlparser import XmlParser, XmlParserError
+import pandas as pd
 
 
-def prepare_toolbox(config_dict):
-    arg_min = config_dict["MIN"]
-    arg_max = config_dict["MAX"]
+POP_SIZE = 20
+CXPB = 0.2
+MUTPB = 0.01
+NGEN = 100
+NCLUSTERS=3
+NSAMPLES=210
 
+class Dataset:
+    def __init__(self):
+        # self.df_raw = pd.read_csv('res/seeds_dataset.csv', header=None,
+        #                  names=["area", "perimeter", "compactness", "length of kernel", "width of kernel",
+        #                         "asymmetry coefficient", "length of kernel groove", "type"])
+        # self.df = self.df_raw.drop(columns=['type'])
+        self.df_raw = pd.read_csv('res/normalizedSeeds.csv', header=None,
+                         names=["index","area", "perimeter", "compactness", "length of kernel", "width of kernel",
+                                "asymmetry coefficient", "length of kernel groove"])
+        self.df = self.df_raw.drop(columns=['index'])
+        self.ncolumns = len(self.df.columns)
+
+    def evaluate(self, individual):
+
+        for x in individual:
+            if x<0:
+                print(x)
+
+
+        clusters = {ncluster:[] for ncluster in range(NCLUSTERS)}
+        # print(clusters)
+
+        centroids = [individual[x:x+self.ncolumns] for x in range(0,len(individual), self.ncolumns)]
+        # print(centroids)
+
+        for item_index, row in self.df.iterrows():
+            point_coords = row.tolist()
+
+            min_dist_index=-1
+            min_dist = None
+            for centroid_index, centroid in enumerate(centroids):
+                dist = self.dist(centroid, point_coords)
+                if min_dist_index == -1:
+                    min_dist_index = centroid_index
+                    min_dist = dist
+                else:
+                    if min_dist > dist:
+                        min_dist = dist
+                        min_dist_index = centroid_index
+
+            clusters[min_dist_index].append(item_index)
+        # print(clusters)
+
+        return -1,
+
+    def dist(self, center_coords:list, point_coords:list):
+        dist = ((center_coords[0]-point_coords[0])**2 + (center_coords[1]-point_coords[1])**2)**0.5
+        for cent_c, point_c in zip(center_coords[2:], point_coords[2:]):
+            dist = (dist**2 + (cent_c-point_c)**2 )**0.5
+        return dist
+
+def prepare_toolbox(dataset: Dataset):
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMin)
 
     toolbox = base.Toolbox()
 
     # typ i zakres genu osobnika
-    toolbox.register("attr_float", random.uniform, arg_min, arg_max)
+    toolbox.register("attr_float", random.uniform, 0., 1.)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, NCLUSTERS*dataset.ncolumns)
 
-    # osobnik to lista attr_float o długości 2
-    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, 2)
+    # toolbox.register("indices", random.sample, range(210), 210)
+    # toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+
+    # toolbox.register("attr_int", random.randint, 0, NSAMPLES)
+    # toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_int, 4)
+
 
     # populacja to lista osobników
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    # wybór funkcji przystosowania #todo: wybór funkcji przystosowania na podstawie config_dict
-    eval_function = None
-    if config_dict["FUNCTION"] == "sin_times_cos":
-        eval_function=sin_times_cos
-    elif config_dict["FUNCTION"] == "sin_times_cos_plus_sum":
-        eval_function=sin_times_cos_plus_sum
-    else:
-        raise NotImplementedError
-
-    toolbox.register("evaluate", eval_function)
+    # wybór funkcji przystosowania
+    toolbox.register("evaluate", dataset.evaluate)
 
     # funkcja kary
+    arg_min = 0
+    arg_max = 1
     def feasible(individual):
-        if arg_min < individual[0] < arg_max and arg_min < individual[1] < arg_max:
-            return True
+        for each in individual:
+            if arg_min < each < arg_max:
+                return True
         return False
 
     def distance(individual):
         # tuple to float
-        fit_val = eval_function(individual)[0]
+        fit_val = dataset.evaluate(individual)[0]
 
         for x in individual:
             if x < arg_min:
@@ -60,26 +116,14 @@ def prepare_toolbox(config_dict):
     toolbox.decorate("evaluate", tools.DeltaPenalty(feasible, 0.0, distance))
 
     # wybór metody krzyżowania
-    if config_dict["CROSSOVER"]["name"] == "cxTwoPoint":
-        toolbox.register("mate", tools.cxTwoPoint)
-    elif config_dict["CROSSOVER"]["name"] == "cxUniform":
-        indpb = config_dict["CROSSOVER"]["indpb"]
-        toolbox.register("mate", tools.cxUniform, indpb=indpb)
-    else:
-        raise NotImplementedError
+    # toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mate", tools.cxUniform, indpb=0.2)
+
 
     #wybór metody mutacji
     # indpb prawdopodobieństwo mutacji atrybutu/genu
-    if config_dict["MUTATION"]["name"] == "mutGaussian":
-        mu = config_dict["MUTATION"]["mu"]
-        sigma = config_dict["MUTATION"]["sigma"]
-        indpb = config_dict["MUTATION"]["indpb"]
-        toolbox.register("mutate", tools.mutGaussian, mu=mu, sigma=sigma, indpb=indpb)
-    elif config_dict["MUTATION"]["name"] == "mutShuffleIndexes":
-        indpb = config_dict["MUTATION"]["indpb"]
-        toolbox.register("mutate", tools.mutShuffleIndexes, indpb=indpb)
-    else:
-        raise NotImplementedError
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.1, indpb=0.2)
+    # toolbox.register("mutate", tools.mutUniformInt, low=0, up=NSAMPLES, indpb=0.2)
 
     # wybór metody selekcji
     toolbox.register("select", tools.selTournament, tournsize=2)
@@ -94,38 +138,20 @@ def prepare_statistics():
 
 
 if __name__ == "__main__":
-    try:
-        config_filename = input("Wpisz nazwę pliku konfiguracyjnego lub pomiń aby użyć config.xml")
-        if not exists(config_filename):
-            input("Nie znaleziono podanego pliku konfiguracyjnego. Zostanie użyty config.xml")
-            config_filename = "config.xml"
+    # random.seed(2)
+    dataset = Dataset()
 
-        parser = XmlParser(config_filename)
-        config_dict = parser.get_config_dict()
-
-        toolbox = prepare_toolbox(config_dict)
-        stats = prepare_statistics()
+    toolbox = prepare_toolbox(dataset)
+    stats = prepare_statistics()
 
 
-        init_pop = toolbox.population(n=config_dict['POP_SIZE'])
-        algorithm = config_dict["ALGORITHM"]
+    init_pop = toolbox.population(POP_SIZE)
 
-        rpop, logbook = None, None
 
-        if algorithm == "eaSimple":
-            rpop, logbook = algorithms.eaSimple(init_pop, toolbox, config_dict['CXPB'], config_dict['MUTPB'], config_dict['NGEN'], stats=stats)
-        elif algorithm == "eaMuPlusLambda":
-            mu = 20
-            lambda_ = 7*mu
-            rpop, logbook = algorithms.eaMuPlusLambda(init_pop, toolbox, mu, lambda_, config_dict['CXPB'], config_dict['MUTPB'], config_dict['NGEN'], stats=stats)
-        # elif algorithm == "CustomSimple":
-        #     main(config_dict, toolbox)
-        else:
-            print(f"algorytm o nazwie {algorithm} nie jest obsługiwany")
+    rpop, logbook = algorithms.eaSimple(init_pop, toolbox, CXPB, MUTPB, NGEN, stats=stats)
 
-        min_ = logbook.select("min")
-        plt.plot(min_)
-        plt.show()
 
-    except XmlParserError as xpe:
-        print("wystąpił błąd podczas parsowania dokumentu konfiguracyjnego")
+    min_ = logbook.select("min")
+    plt.plot(min_)
+    plt.show()
+
